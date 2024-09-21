2c7387472aadf0eb8e27b1fab31be86db7390e0d
carloscordova.dev <cordovamanuel22@icloud.com>
2024-09-21 07:37:19 -0700

    feat: :sparkles: introduced pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Splashscreen from './components/Splashscreen'
+
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <div>
-      <Splashscreen/>
+      <Home/>
     </div>
   )
 }
